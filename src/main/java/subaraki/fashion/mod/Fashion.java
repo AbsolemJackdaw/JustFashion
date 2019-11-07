fca75429caa6db55f632177e6d12593a20d0fec5
ArtixAllMighty <robbeenaxel@hotmail.com>
2019-11-07 13:08:23 +0100

    removed old code

--- src/main/java/subaraki/fashion/mod/Fashion.java
+++ src/main/java/subaraki/fashion/mod/Fashion.java
@@ -3,26 +3,18 @@ package subaraki.fashion.mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.client.gui.ScreenManager;
-import net.minecraft.inventory.container.ContainerType;
-import net.minecraftforge.common.extensions.IForgeContainerType;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ObjectHolder;
 import subaraki.fashion.capability.FashionCapability;
 import subaraki.fashion.client.ClientReferences;
 import subaraki.fashion.client.ResourcePackReader;
 import subaraki.fashion.client.event.EventRegistryClient;
 import subaraki.fashion.client.event.forge_bus.KeyRegistry;
 import subaraki.fashion.network.NetworkHandler;
-import subaraki.fashion.screen.WardrobeContainer;
-import subaraki.fashion.screen.WardrobeScreen;
 import subaraki.fashion.server.event.EventRegistryServer;
 
 @Mod(Fashion.MODID)
@@ -55,22 +47,6 @@ public class Fashion {
         new EventRegistryClient();
         new ResourcePackReader();
         new KeyRegistry().registerKey();
-//        ScreenManager.registerFactory(ObjectHolders.WARDROBECONTAINER, WardrobeScreen::new);
         ClientReferences.loadLayers();
-    }
-    
-//    @ObjectHolder(Fashion.MODID)
-//    public static class ObjectHolders {
-//
-//        @ObjectHolder("wardrobe_container")
-//        public static final ContainerType<WardrobeContainer> WARDROBECONTAINER = null;
-//    }
-//    
-//    @SubscribeEvent
-//    public static void registerContainers(RegistryEvent.Register<ContainerType<?>> event) {
-//
-//        event.getRegistry().register(IForgeContainerType.create((windowId, inv, data) -> new WardrobeContainer(ObjectHolders.WARDROBECONTAINER, windowId))
-//                .setRegistryName("wardrobe_container"));
-//
-//    }
+    }  
 }
